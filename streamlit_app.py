e3de2e80876d65b68b22c0915637194e36ab6475
mangoman18 <45697884+mangoman18@users.noreply.github.com>
2022-12-26 14:41:56 -0800

    Fixed typos

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit
 streamlit.title("My Parents Healthy Diner")
 streamlit.header('New breakfast Menu')
 streamlit.text('Uh oh Nothin to show here')
-steamlit.text('On intermittent fasting!')
+streamlit.text('On intermittent fasting!')
