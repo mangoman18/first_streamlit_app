7999e881dfded2f7a957a7bd2eb24ffe561314f0
mangoman18 <45697884+mangoman18@users.noreply.github.com>
2022-12-26 14:55:43 -0800

    use pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 import streamlit
+import pandas
 streamlit.title("My Parents Healthy Diner")
 streamlit.header('New breakfast Menu')
 streamlit.text('Uh oh Nothin to show here')
 streamlit.text('On intermittent fasting!')
-streamlit.text('going to try again')
-streamlit.text('🍞 Basic Bread')
+streamlit.text('Ok Will try again going to try again')
+#streamlit.text('🍞 Basic Bread')
+breakfastmenu = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+streamlit.dataframe(breakfastmenu)
