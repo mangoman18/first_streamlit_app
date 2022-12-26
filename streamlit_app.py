2b78c153658ec8d4fefbd23140283a3c563d258e
mangoman18 <45697884+mangoman18@users.noreply.github.com>
2022-12-26 14:41:08 -0800

    Menu changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,5 @@
 import streamlit
 streamlit.title("My Parents Healthy Diner")
+streamlit.header('New breakfast Menu')
+streamlit.text('Uh oh Nothin to show here')
+steamlit.text('On intermittent fasting!')
